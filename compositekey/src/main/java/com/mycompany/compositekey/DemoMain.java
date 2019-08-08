34bce956bd0d63c29da388f2fe23a7243f86fba8
Nihar3011 <patelnihar1997.pn@gmail.com>
2019-08-08 18:11:21 +0530

    Composite key project added

---
+++ compositekey/src/main/java/com/mycompany/compositekey/DemoMain.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.compositekey;
+
+import com.mycompany.compositekey.model.UserProfile;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+/**
+ *
+ * @author nihar
+ */
+public class DemoMain {
+
+
+    public static void main(String[] args) {
+       EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+
+        DemoMain.createUser(entityManager);
+        JPAUtil.shutdown();
+    }
+    
+    public static void createUser(EntityManager entityManager) {
+        
+       
+        entityManager.getTransaction().begin();
+        entityManager.merge(new UserProfile("Nihar ", "npatel@argusof.in", "7985454"));
+        entityManager.getTransaction().commit();
+        entityManager.close();
+       
+    }
+
+}
